2d32f2d09041650bbf504dab6fad242f9971c1d1
Vidhu Kant Sharma <bokuwakanojogahoshii@yahoo.com>
2020-12-06 20:35:09 +0530

    created a somewhat decent template site

--- static/scripts/autohide-navbar.js
+++ static/scripts/autohide-navbar.js
@@ -2,9 +2,9 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("head").style.top = "10px";
+    document.getElementById("head").style.top = "0";
   } else {
-    document.getElementById("head").style.top = "-70px";
+    document.getElementById("head").style.top = "-50px";
   }
   prevScrollpos = currentScrollPos;
 } 
