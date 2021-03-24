5f6da9930551d749980f814434a375713a4b23e2
Vidhu Kant Sharma <bokuwakanojogahoshii@yahoo.com>
2021-03-24 21:07:43 +0530

    fixed the script to properly open and close the popup form

--- static/scripts/popups-handler.js
+++ static/scripts/popups-handler.js
@@ -2,5 +2,11 @@ const popupForm = document.getElementById("popup-form-container");
 
 function showPopupForm() {
 	popupForm.style.display = "flex";
-}
 
+	// hide popup when background is clicked
+	popupForm.addEventListener('click', (e) => {
+		if (e.target.id == "popup-form-container") {
+			popupForm.style.display = "none";
+		}
+	});
+}
